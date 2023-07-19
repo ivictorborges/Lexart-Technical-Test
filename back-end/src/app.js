db11c6da12eaba6d88992a855643d41f9690876a
Victor <v_tord@hotmail.com>
2023-07-19 20:52:09 -0300

    update

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -4,13 +4,9 @@ require('express-async-errors');
 const messagesRouter = require('./router/messagesRouter');
 
 const app = express();
-app.use(express.static('public'))
+
 app.use(cors());
 app.use(express.json());
-
-app.get('/', (_req, res) => {
-    res.send('Welcome to Chatbot API !')
-  });
 
 app.use('/messages', messagesRouter);
 
