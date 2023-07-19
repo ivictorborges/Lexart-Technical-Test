2915696dec3db7627f4a09ebf5ee16bf6382f43f
Victor <v_tord@hotmail.com>
2023-07-19 16:25:36 -0300

    feat: install dotenv

--- back-end/src/model/connection.js
+++ back-end/src/model/connection.js
@@ -1,11 +1,13 @@
 const mysql = require('mysql2/promise');
+const dotenv = require('dotenv');
+dotenv.config();
 
 const connection = mysql.createPool({
-    port: 3306,
-    host: 'localhost',
-    user: 'root',
-    password: 'password',
-    database: 'Chatbot'
+    port: process.env.PORT || 3306,
+    host: process.env.HOST || 'localhost',
+    user: process.env.USER || 'root',
+    password: process.env.PASSWORD || 'password',
+    database: process.env.DATABASE || 'Chatbot'
   });
 
 module.exports = connection;
